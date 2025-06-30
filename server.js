b90cb95d91fbb3b859d0cfe5664dd3d0d13f652b
David Ngacha <davidngacha01@gmail.com>
2025-06-30 12:10:58 +0300

    initialized creative commerce api

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
